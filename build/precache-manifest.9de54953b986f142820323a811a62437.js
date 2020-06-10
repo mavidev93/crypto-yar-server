ae824a191fc0dbdf29e6a28fdac9d30f6bc2e0d5
Your Name <mavidev93@gmail.com>
2020-06-10 22:10:14 +0430

    after adding build to server

---
+++ build/precache-manifest.9de54953b986f142820323a811a62437.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70c2fc321aff917ea1d9fed179ef5204",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11fef7345f7198b26330",
+    "url": "/static/css/main.e1937d10.chunk.css"
+  },
+  {
+    "revision": "b45c2e82a8a85d93f900",
+    "url": "/static/js/2.f5742d50.chunk.js"
+  },
+  {
+    "revision": "320cd72d997b461965b9d71e8f908771",
+    "url": "/static/js/2.f5742d50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11fef7345f7198b26330",
+    "url": "/static/js/main.8bd5e0dc.chunk.js"
+  },
+  {
+    "revision": "9114d4ac523258ea563e",
+    "url": "/static/js/runtime-main.7fb9cef2.js"
+  },
+  {
+    "revision": "ef197892fde91a7299a8f0d32fa66a47",
+    "url": "/static/media/Vazir-Light-WOL.ef197892.woff"
+  }
+]);
--- index.js
+++ index.js
@@ -36,12 +36,12 @@ db.on("error", console.error.bind(console, "MongoDB connection error:"));
 app.use("/auth", authRouter);
 
 app.use("/crypto", cryptoRouter);
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+// if (process.env.NODE_ENV === "production") {
+app.use(express.static("build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
+});
+// }
 
 app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
